438a3962101a0e242d79596a00246b6743d77419
Fernando Soares <fernando.hfs94@gmail.com>
2023-08-15 20:02:16 -0300

    fix(import): adjust to get correctly

--- src/domain/forum/application/use-cases/answer-question.use-case.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.use-case.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from './answer-question.use-case'
 import { AnswersRepository } from '../repositories/answers.repository'
-import { Answer } from '../entities/answer'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 const fakeAnswerRepository: AnswersRepository = {
   create: async (answer: Answer) => {},
--- src/domain/forum/application/use-cases/answer-question.use-case.ts
+++ src/domain/forum/application/use-cases/answer-question.use-case.ts
@@ -1,5 +1,5 @@
-import { UniqueEntityID } from '../../core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers.repository'
 
 interface AnswerQuestionInput {
