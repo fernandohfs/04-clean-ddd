fefcb827c717dd78c51eab045cf9e10d61e0de50
Fernando Soares <fernando.hfs94@gmail.com>
2023-08-16 07:56:03 -0300

    test: adjust name

--- src/domain/forum/application/use-cases/create-question.use-case.spec.ts
+++ src/domain/forum/application/use-cases/create-question.use-case.spec.ts
@@ -10,7 +10,7 @@ describe('Create Question', () => {
     sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  test('it should be able to create an answer', async () => {
+  test('it should be able to create a question', async () => {
     const { question } = await sut.execute({
       authorId: '1',
       title: 'Nova pergunta',
